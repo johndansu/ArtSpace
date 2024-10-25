b7c61da2d0c3f08da29df1620de1ab9914f97439
John Dansu <dansu.jw@gmail.com>
2024-10-25 18:14:27 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+function performSearch() {
+    const query = document.querySelector('.form-inline .form-control').value;
+    // Implement your search logic here
+    alert('Searching for: ' + query); // Replace with actual search logic
+}
